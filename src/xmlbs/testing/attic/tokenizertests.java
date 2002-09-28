0485ef608055049b5375ccb89eef8773d74600e2
Remco van 't Veer <rwvtveer@xs4all.nl>
2002-09-28 13:34:08 +0000

    added text tests

--- src/xmlbs/testing/attic/tokenizertests.java
+++ src/xmlbs/testing/attic/tokenizertests.java
@@ -34,7 +34,90 @@ public class TokenizerTests extends TestCase
     public void testText ()
     throws IOException
     {
-	assertTrue("assert doesn't work", true);
+	{
+	    String d = " ";
+	    Tokenizer tokenizer = new Tokenizer(d);
+	    List tokens = tokenizer.readAllTokens();
+
+	    assertTrue("didn't read 1 token from '"+d+"' but "+tokens.size(),
+		    tokens.size() == 1);
+	    assertTrue("didn't read a text token from '"+d+"'",
+		    tokens.get(0) instanceof TextToken);
+
+	    TextToken tok = (TextToken) tokens.get(0);
+	    String data = tok.getData();
+
+	    assertTrue("didn't read ' ' from '"+d+"' but '"+data+"'",
+		    data.equals(" "));
+	}
+	{
+	    String d = "<";
+	    Tokenizer tokenizer = new Tokenizer(d);
+	    List tokens = tokenizer.readAllTokens();
+
+	    assertTrue("didn't read 1 token from '"+d+"' but "+tokens.size(),
+		    tokens.size() == 1);
+
+	    TextToken tok = (TextToken) tokens.get(0);
+	    String data = tok.getData();
+
+	    assertTrue("didn't read '<' from '"+d+"' but '"+data+"'",
+		    data.equals("<"));
+
+	    assertTrue("didn't get '&lt;' from '"+d+"' but '"+tok+"'",
+		    tok.toString().equals("&lt;"));
+	}
+	{
+	    String d = "<blabla<>";
+	    Tokenizer tokenizer = new Tokenizer(d);
+	    List tokens = tokenizer.readAllTokens();
+
+	    assertTrue("didn't read 1 token from '"+d+"' but "+tokens.size(),
+		    tokens.size() == 1);
+
+	    TextToken tok = (TextToken) tokens.get(0);
+	    String data = tok.getData();
+
+	    assertTrue("didn't read '<blabla<>' from '"+d+"' but '"+data+"'",
+		    data.equals("<blabla<>"));
+
+	    assertTrue("didn't get '&lt;blabla&lt;&gt;' from '"+d+"' but '"+tok+"'",
+		    tok.toString().equals("&lt;blabla&lt;&gt;"));
+	}
+	{
+	    String d = "<<blabla>>";
+	    Tokenizer tokenizer = new Tokenizer(d);
+	    List tokens = tokenizer.readAllTokens();
+
+	    assertTrue("didn't read 3 token from '"+d+"' but "+tokens.size(),
+		    tokens.size() == 3);
+
+	    TextToken tok0 = (TextToken) tokens.get(0);
+	    String data0 = tok0.getData();
+	    TextToken tok1 = (TextToken) tokens.get(2);
+	    String data1 = tok1.getData();
+
+	    assertTrue("didn't read '<' from '"+d+"' but '"+data0+"'",
+		    data0.equals("<"));
+	    assertTrue("didn't read '>' from '"+d+"' but '"+data1+"'",
+		    data1.equals(">"));
+	}
+	{
+	    String d = ">'blabla\"<";
+	    Tokenizer tokenizer = new Tokenizer(d);
+	    List tokens = tokenizer.readAllTokens();
+
+	    assertTrue("didn't read 1 token from '"+d+"' but "+tokens.size(),
+		    tokens.size() == 1);
+
+	    TextToken tok = (TextToken) tokens.get(0);
+	    String data = tok.getData();
+
+	    assertTrue("didn't read '>'blabla\"<' from '"+d+"' but '"+data+"'",
+		    data.equals(d));
+	    assertTrue("didn't get '&gt;&apos;blabla&quot;&lt;' from '"+d+"' but '"+tok.toString()+"'",
+		    tok.toString().equals("&gt;&apos;blabla&quot;&lt;"));
+	}
     }
 
     public void testComment ()
